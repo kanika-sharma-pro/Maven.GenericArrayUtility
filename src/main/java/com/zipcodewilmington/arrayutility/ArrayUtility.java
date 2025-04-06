975819cf92ba95870440ea15d2639ddbfffd2925
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-06 14:06:23 -0400

    did 3 but due to errors not able to run test

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -18,27 +18,35 @@ public class ArrayUtility <T> {
     //    return getNumberOfOccurrences(this.array, valueToEvaluate);
 
 
+    private T[] concatenate(T[] arrayToMerge) {
+        T[] result = Arrays.copyOf(this.inputArray, this.inputArray.length + arrayToMerge.length);
+        System.arraycopy(arrayToMerge, 0, result, this.inputArray.length, arrayToMerge.length);
+        return null;
+
+    }
+
+
     private Integer getNumberOfOccurrences(T[] array, T valueToEvaluate) {
+
+        Integer count = 0;
+        //count the total occurrences of stored array
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(valueToEvaluate)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private Integer getNumberOfOccurrences(T valueToEvaluate) {
         return getNumberOfOccurrences(this.inputArray, valueToEvaluate);
     }
-
-    private T[] concatenate (T[] arrayToMerge) {
-        T[] result = Arrays.copyOf(this.inpuArray, )
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
+public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+    T[] result = concatenate(arrayToMerge);
+    return countDuplicatesInMerge(result, valueToEvaluate);
+}
 
 //public T countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
 //    }
@@ -59,7 +67,7 @@ public class ArrayUtility <T> {
 //                    count++;
 //                }
 //            }
-//            //count occurence in array to merge
+//            //count occurrence in array to merge
 //            for (int j = 0; j <arrayToMerge.length; j++) {
 //                if (array[j] != null && array[j].equals(valueToEvaluate)){
 //                    count++;
